a2d2f87f7240c31435262658bc90d29a848e0c99
Joshua Plicque <plicjo@gmail.com>
2020-05-25 11:33:43 -0400

    Rename method

--- cyclic_rotation.rb
+++ cyclic_rotation.rb
@@ -7,7 +7,7 @@ end
 def cyclic_rotation(initial_array, rotations)
   return initial_array if initial_array.empty?
 
-  (1..rotations).inject(initial_array) do |current_array, _|
+  (1..rotations).reduce(initial_array) do |current_array, _|
     last_element = current_array.slice(-1)
     array_minus_last_element = current_array.slice(0..-2)
     [last_element] + array_minus_last_element
